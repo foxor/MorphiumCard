bb846b3211e7b92309bf4b490748543f4b1e9f5c
Isaac James <izach.james@gmail.com>
2013-10-23 20:48:38 -0700

    Whoops

--- Assets/GlobalRequire.cs
+++ Assets/GlobalRequire.cs
@@ -4,10 +4,12 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 
-[RequireComponent(typeof(UI))]
-[RequireComponent(typeof(TurnManager))]
-[RequireComponent(typeof(Server))]
-[RequireComponent(typeof(Client))]
-[RequireComponent(typeof(Chooser))]
 public class GlobalRequire: MonoBehaviour {
+	public void Awake() {
+		gameObject.AddComponent<UI>();
+		gameObject.AddComponent<TurnManager>();
+		gameObject.AddComponent<Server>();
+		gameObject.AddComponent<Client>();
+		gameObject.AddComponent<Chooser>();
+	}
 }
