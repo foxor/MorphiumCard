66ab3047c7557beb4290041d75e93bb3fcb4211c
Isaac James <izach.james@gmail.com>
2013-10-23 20:40:10 -0700

    Global Require

---
+++ Assets/GlobalRequire.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+[RequireComponent(typeof(UI))]
+[RequireComponent(typeof(TurnManager))]
+[RequireComponent(typeof(Server))]
+[RequireComponent(typeof(Client))]
+[RequireComponent(typeof(Chooser))]
+public class GlobalRequire: MonoBehaviour {
+}
