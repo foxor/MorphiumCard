a048c93c3a04b4f15631ff1cb5da8c4ab25a1c1d
Isaac James <izach.james@gmail.com>
2013-11-07 08:24:06 -0800

    Refactor targeting

--- Assets/UI/Targeting.cs
+++ Assets/UI/Targeting.cs
@@ -4,88 +4,11 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 
-public enum TargetTypeFlag {
-	Lane = 1 << 0,
-	Morphid = 1 << 1,
-	Minion = 1 << 2,
-	Enemy = 1 << 3,
-	Friendly = 1 << 4,
-	Empty = 1 << 5
-}
-
-public enum TargetingType {
-	Single,
-	All
-}
-
-public class TargetingRequirements {
-	public int TargetFlags;
-	public TargetingType TargetingType;
-	
-	public TargetingRequirements(int Flags, TargetingType TargetingType) {
-		this.TargetFlags = Flags;
-		this.TargetingType = TargetingType;
-	}
-	
-	public bool HasFlag(TargetTypeFlag flag) {
-		return (TargetFlags & (int)flag) > 0;
-	}
-	
-	public bool LaneAllowed(Lane l) {
-		return HasFlag(TargetTypeFlag.Lane) &&
-			(!HasFlag(TargetTypeFlag.Empty) || Minion.IsDead(l.FriendlyMinion(GameState.ActiveMorphid.GUID)));
-	}
-	
-	public bool MorphidAllowed(Morphid m) {
-		return HasFlag(TargetTypeFlag.Morphid) &&
-			m != null &&
-			((HasFlag(TargetTypeFlag.Friendly) && m.GUID == GameState.ActiveMorphid.GUID) ||
-			(HasFlag(TargetTypeFlag.Enemy) && m.GUID != GameState.ActiveMorphid.GUID));
-	}
-	
-	public bool MinionAllowed(Minion m) {
-		return HasFlag(TargetTypeFlag.Minion) &&
-			m != null &&
-			((HasFlag(TargetTypeFlag.Friendly) && m.IsFriendly(GameState.ActiveMorphid.GUID)) ||
-			(HasFlag(TargetTypeFlag.Enemy) && m.IsEnemy(GameState.ActiveMorphid.GUID)));
-	}
-	
-	public IEnumerable<string> AllTargets(string guid) {
-		foreach (Lane lane in GameState.Singleton.Lanes) {
-			if (LaneAllowed(lane)) {
-				yield return lane.GUID;
-			}
-			foreach (Minion minion in lane.Minions) {
-				if (MinionAllowed(minion)) {
-					yield return minion.GUID;
-				}
-			}
-		}
-		foreach (Morphid morphid in GameState.Singleton.Morphids) {
-			if (MorphidAllowed(morphid)) {
-				yield return morphid.GUID;
-			}
-		}
-	}
-	
-	public bool TargetAllowed(string guid) {
-		Lane lane = GameState.GetLane(guid);
-		Morphid morphid = GameState.GetMorphid(guid);
-		Minion minion = GameState.GetMinion(guid);
-		if (lane != null) {
-			return LaneAllowed(lane);
-		}
-		if (morphid != null) {
-			return MorphidAllowed(morphid);
-		}
-		if (minion != null) {
-			return MinionAllowed(minion);
-		}
-		if (TargetingType == TargetingType.All) {
-			return true;
-		}
-		return false;
-	}
+public enum TargetingModes {
+	Inactive,
+	Transitional,
+	ClickTargeting,
+	DragTargeting,
 }
 
 public class Target {
