cd6aa66866f7073c753b389dd619cede7215012c
Isaac James <izach.james@gmail.com>
2013-11-15 08:51:08 -0800

    Update cardbutton and format

--- Assets/UI/CardButton.cs
+++ Assets/UI/CardButton.cs
@@ -2,62 +2,63 @@
 using System.Collections;
 
 public class CardButton : Button {
-	public int CardIndex;
+    protected const int ART_WIDTH = 500;
+    protected const int ART_HEIGHT = 770;
+    public int CardIndex;
+    protected CardMarker Card;
+    protected CostFieldMarker CardCost;
+    protected NameFieldMarker CardName;
+    protected TextFieldMarker CardText;
+    protected int oldLeft;
+    protected int oldTop;
+    protected float zPos;
+    
+    public CardButton (int CardIndex, Region source, CardMarker card) : base(source) {
+        this.CardIndex = CardIndex;
+        Action = UI.Singleton.PickupCard (CardIndex);
+        Card = card;
+        CardCost = card.GetComponentInChildren<CostFieldMarker> ();
+        CardName = card.GetComponentInChildren<NameFieldMarker> ();
+        CardText = card.GetComponentInChildren<TextFieldMarker> ();
+        Left = (int)Card.transform.position.x;
+        Top = (int)Card.transform.position.y;
+        Width = ART_WIDTH;
+        Height = ART_HEIGHT;
+        zPos = Card.transform.position.z;
+    }
 
-	protected CardMarker Card;
-	protected CostFieldMarker CardCost;
-	protected NameFieldMarker CardName;
-	protected TextFieldMarker CardText;
+    public void OnPickup () {
+        oldLeft = Left;
+        oldTop = Top;
+    }
 
-	protected int oldLeft;
-	protected int oldTop;
-	protected float zPos;
-	
-	public CardButton(int CardIndex, Region source, CardMarker card) : base(source) {
-		this.CardIndex = CardIndex;
-		Action = UI.Singleton.PickupCard(CardIndex);
-		Card = card;
-		CardCost = card.GetComponentInChildren<CostFieldMarker>();
-		CardName = card.GetComponentInChildren<NameFieldMarker>();
-		CardText = card.GetComponentInChildren<TextFieldMarker>();
-		Left = (int)Card.transform.position.x;
-		Top = (int)Card.transform.position.y;
-		zPos = Card.transform.position.z;
-	}
+    public void OnDrop () {
+        Left = oldLeft;
+        Top = oldTop;
+    }
 
-	public void OnPickup() {
-		oldLeft = Left;
-		oldTop = Top;
-	}
-
-	public void OnDrop() {
-		Left = oldLeft;
-		Top = oldTop;
-	}
-
-	public bool isEnabled() {
-		return Morphid.Cards != null && 
-			Morphid.Cards[CardIndex] != null &&
-			Morphid.Cards[CardIndex].Cost <= Morphid.LocalPlayer.Morphium;
-	}
-	
-	protected override void DrawInner() {
-		Card.transform.position = new Vector3(Left, Top, zPos);
-		if (Morphid.Cards != null && Morphid.Cards[CardIndex] != null) {
-			Text = Morphid.Cards[CardIndex].Name +
-				" (" + Morphid.Cards[CardIndex].Cost + ")\n" +
-				Morphid.Cards[CardIndex].Text;
-			CardCost.Text = Morphid.Cards[CardIndex].Cost.ToString();
-			CardName.Text = Morphid.Cards[CardIndex].Name;
-			CardText.Text = Morphid.Cards[CardIndex].Text;
-		}
-		else {
-			Text = "Empty";
-		}
-		if (ContainsMouse() != null && Input.GetMouseButton(0) && Enabled) {
-			Action();
-			invalid = true;
-		}
-		base.DrawInner();
-	}
+    public bool isEnabled () {
+        return Morphid.Cards != null && 
+            Morphid.Cards [CardIndex] != null &&
+            Morphid.Cards [CardIndex].Cost <= Morphid.LocalPlayer.Morphium;
+    }
+    
+    protected override void DrawInner () {
+        Card.transform.position = new Vector3 (Left, Top, zPos);
+        if (Morphid.Cards != null && Morphid.Cards [CardIndex] != null) {
+            Text = Morphid.Cards [CardIndex].Name +
+                " (" + Morphid.Cards [CardIndex].Cost + ")\n" +
+                Morphid.Cards [CardIndex].Text;
+            CardCost.Text = Morphid.Cards [CardIndex].Cost.ToString ();
+            CardName.Text = Morphid.Cards [CardIndex].Name;
+            CardText.Text = Morphid.Cards [CardIndex].Text;
+        } else {
+            Text = "Empty";
+        }
+        if (ContainsMouse () != null && Input.GetMouseButton (0) && Enabled) {
+            Action ();
+            invalid = true;
+        }
+        base.DrawInner ();
+    }
 }
