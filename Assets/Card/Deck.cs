159cec8f894d7543b1e66ded4afe3aa612db027d
Isaac James <izach.james@gmail.com>
2013-11-26 23:36:06 -0800

    Format doc

--- Assets/Card/Deck.cs
+++ Assets/Card/Deck.cs
@@ -9,34 +9,33 @@ using ProtoBuf.Meta;
 [Serializable]
 [ProtoContract]
 public class Deck {
-	public const int MAX_CARDS = 15;
-	
-	protected static Importer Importer = new Importer();
-	
-	[SerializeField]
-	[ProtoMember(1)]
-	public Card[] Cards;
-	
-	[SerializeField]
-	[ProtoMember(2)]
-	public int Index;
-	
-	[SerializeField]
-	[ProtoMember(3)]
-	public Slot Slot;
-	
-	public void Shuffle() {
-		Cards = Importer.CardsBySlot(Slot).OrderBy(x => UnityEngine.Random.Range(0f, 1f)).ToArray();
-	}
-	
-	public Card Draw() {
+    public const int MAX_CARDS = 15;
+    protected static Importer Importer = new Importer ();
+    [SerializeField]
+    [ProtoMember(1)]
+    public Card[]
+        Cards;
+    [SerializeField]
+    [ProtoMember(2)]
+    public int
+        Index;
+    [SerializeField]
+    [ProtoMember(3)]
+    public Slot
+        Slot;
+    
+    public void Shuffle () {
+        Cards = Importer.CardsBySlot(Slot).OrderBy(x => UnityEngine.Random.Range(0f, 1f)).ToArray();
+    }
+    
+    public Card Draw () {
         if (Index >= Cards.Length) {
             Cards = Cards.OrderBy(x => UnityEngine.Random.Range(0f, 1f)).ToArray();
             Index = 0;
         }
-		if (Cards != null) {
-			return Cards[Index++];
-		}
-		return null;
-	}
+        if (Cards != null) {
+            return Cards[Index++];
+        }
+        return null;
+    }
 }
