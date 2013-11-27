fa2cb67dd9ec856eaee07ddd9c19804548a1fe03
Isaac James <izach.james@gmail.com>
2013-11-26 23:35:42 -0800

    Recycle decks

--- Assets/Card/Deck.cs
+++ Assets/Card/Deck.cs
@@ -30,7 +30,11 @@ public class Deck {
 	}
 	
 	public Card Draw() {
-		if (Cards != null && Index <= Cards.Count() - 1) {
+        if (Index >= Cards.Length) {
+            Cards = Cards.OrderBy(x => UnityEngine.Random.Range(0f, 1f)).ToArray();
+            Index = 0;
+        }
+		if (Cards != null) {
 			return Cards[Index++];
 		}
 		return null;
