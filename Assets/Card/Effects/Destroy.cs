c27b9351efd13d4102c2de6a895f8c27baa53134
Isaac James <izach.james@gmail.com>
2013-12-02 16:22:26 -0800

    Destroy Effect

---
+++ Assets/Card/Effects/Destroy.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using ProtoBuf;
+using ProtoBuf.Meta;
+
+[Serializable]
+[ProtoContract]
+public class Destroy : Effect {
+    public const string CSV_NAME = "Destroy";
+    
+    [SerializeField]
+    [ProtoMember(3)]
+    public Expression Magnitude;
+    
+    public override void Apply (string target) {
+        SubstitutionExpression.Substitutions[CSV_NAME] = GameState.GetMinion(target);
+        GameState.RemoveMinion(target);
+    }
+}
