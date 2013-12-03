67c323fee90ed0570fef1b1050e21b5b580cc7c0
Isaac James <izach.james@gmail.com>
2013-12-03 00:24:40 -0800

    Fix roll over and pulverize

--- Assets/Card/Parser/SubstitutionExpression.cs
+++ Assets/Card/Parser/SubstitutionExpression.cs
@@ -1,11 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
 
+public delegate object Poll();
+
 public class SubstitutionExpression : Expression {
     public static Dictionary<string, object> Substitutions;
+    public static Dictionary<string, Poll> Searches;
 
     static SubstitutionExpression() {
         Substitutions = new Dictionary<string, object>();
+        Searches = new Dictionary<string, Poll>();
+
+        Searches[Weight.ARGUMENT_NAME] = () => GameState.ActiveMorphid.Weight;
     }
 
     protected string substitution;
@@ -14,7 +20,16 @@ public class SubstitutionExpression : Expression {
         this.substitution = substitution;
     }
 
+    protected object Value {
+        get {
+            if (Searches.ContainsKey(substitution)) {
+                return Searches[substitution]();
+            }
+            return Substitutions[substitution];
+        }
+    }
+
     public override int Evaluate() {
-        return (int)Substitutions[substitution];
+        return (int)Value;
     }
 }
