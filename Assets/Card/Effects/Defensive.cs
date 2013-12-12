630b6189e1473d09e3b22da8b5664a100d3bc520
Isaac James <izach.james@gmail.com>
2013-12-12 13:38:15 -0800

    Serialize defensive

--- Assets/Card/Effects/Defensive.cs
+++ Assets/Card/Effects/Defensive.cs
@@ -1,6 +1,13 @@
-﻿using UnityEngine;
+using UnityEngine;
+using System;
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using ProtoBuf;
+using ProtoBuf.Meta;
 
+[Serializable]
+[ProtoContract]
 public class Defensive : Effect {
     public const string CSV_NAME = "Defensive";
 
