0747449a95d10e102e0c61306b71e496f0302cdb
Isaac James <izach.james@gmail.com>
2014-03-15 12:37:25 -0700

    Avalanche and Drill Buddy

---
+++ Assets/Card/Effects/Avalanche.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Card.Effects
+{
+    class Avalanche : Effect
+    {
+        public static DynamicProvider DamageMag = () => 7;
+
+        public Avalanche(string text) : base(text) { }
+
+        protected override IEnumerable<DynamicProvider> TemplatingArguments()
+        {
+            yield return DamageMag;
+        }
+
+        protected override IEnumerable<TargetTypeFlag> TargetTypeFlags()
+        {
+            yield return TargetTypeFlag.Enemy;
+            yield return TargetTypeFlag.Minion;
+        }
+
+        public override void Apply(string guid)
+        {
+            GameState.DamageGuid(guid, DamageMag());
+        }
+
+        public override int Cost()
+        {
+            return 4;
+        }
+
+        public override TargetingType TargetingType()
+        {
+            return global::TargetingType.Single;
+        }
+    }
+}
---
+++ Assets/Card/Effects/DrillBuddy.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Card.Effects
+{
+    class DrillBuddy : Effect
+    {
+        public static DynamicProvider AttackMag = () => GameState.ActiveMorphid.Weight;
+        public static DynamicProvider DefenseMag = () => GameState.ActiveMorphid.Weight * 3;
+
+        public DrillBuddy(string text) : base(text) { }
+
+        protected override IEnumerable<DynamicProvider> TemplatingArguments()
+        {
+            yield return AttackMag;
+            yield return DefenseMag;
+        }
+
+        protected override IEnumerable<TargetTypeFlag> TargetTypeFlags()
+        {
+            yield return TargetTypeFlag.Empty;
+            yield return TargetTypeFlag.Lane;
+        }
+
+        public override void Apply(string guid)
+        {
+            GameState.SummonMinion(guid, AttackMag(), DefenseMag(), false, false);
+        }
+
+        public override int Cost()
+        {
+            return 4;
+        }
+
+        public override TargetingType TargetingType()
+        {
+            return global::TargetingType.Single;
+        }
+    }
+}
