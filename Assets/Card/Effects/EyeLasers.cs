650e2c0be2a8587235f5be853460303e9d2c2ebe
Isaac James <izach.james@gmail.com>
2014-03-16 10:17:02 -0700

    Eye Lasers

---
+++ Assets/Card/Effects/EyeLasers.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EyeLasers : Effect {
+
+    protected int lastCostCheck;
+    public DynamicProvider Damage;
+
+    public EyeLasers(string text) : base(text) {
+        Damage = () => lastCostCheck * 2;
+    }
+
+    protected override System.Collections.Generic.IEnumerable<DynamicProvider> TemplatingArguments ()
+    {
+        yield return Damage;
+    }
+
+    protected override System.Collections.Generic.IEnumerable<TargetTypeFlag> TargetTypeFlags ()
+    {
+        yield return TargetTypeFlag.Enemy;
+        yield return TargetTypeFlag.Friendly;
+        yield return TargetTypeFlag.Minion;
+        yield return TargetTypeFlag.Morphid;
+    }
+
+    public override void Apply (string guid)
+    {
+        GameState.DamageGuid(guid, Damage());
+    }
+
+    public override int Cost ()
+    {
+        lastCostCheck = GameState.ActiveMorphid.Morphium;
+        return lastCostCheck;
+    }
+
+    public override TargetingType TargetingType ()
+    {
+        return global::TargetingType.Single;
+    }
+}
