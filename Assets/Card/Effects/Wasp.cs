d7c52e1b5891f6ac5334c08ff8ba3a012b069f39
Isaac James <izach.james@gmail.com>
2014-03-16 11:00:02 -0700

    Wasp

---
+++ Assets/Card/Effects/Wasp.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wasp : Effect {
+    public static DynamicProvider Attack = () => 2;
+    public static DynamicProvider Defense = () => 6;
+
+    public Wasp(string text) : base(text) {}
+
+    protected override System.Collections.Generic.IEnumerable<DynamicProvider> TemplatingArguments ()
+    {
+        yield return Attack;
+        yield return Defense;
+    }
+
+    protected override System.Collections.Generic.IEnumerable<TargetTypeFlag> TargetTypeFlags ()
+    {
+        yield return TargetTypeFlag.Empty;
+        yield return TargetTypeFlag.Lane;
+    }
+
+    public override void Apply (string guid)
+    {
+        GameState.SummonMinion(guid, Attack(), Defense(), new MinionBuilder() {Scrounge = true});
+    }
+
+    public override int Cost ()
+    {
+        return 2;
+    }
+
+    public override TargetingType TargetingType ()
+    {
+        return global::TargetingType.Single;
+    }
+}
