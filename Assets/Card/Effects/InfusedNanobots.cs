410ac7af46782aa0316ea6940c49286910b73c83
Isaac James <izach.james@gmail.com>
2014-03-16 10:32:09 -0700

    Infused Nanobots

---
+++ Assets/Card/Effects/InfusedNanobots.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InfusedNanobots : Effect {
+    public static DynamicProvider Repair = () => GameState.ActiveMorphid.Engine;
+
+    public InfusedNanobots(string text) : base(text) {}
+
+    protected override System.Collections.Generic.IEnumerable<DynamicProvider> TemplatingArguments ()
+    {
+        yield return Repair;
+    }
+
+    protected override System.Collections.Generic.IEnumerable<TargetTypeFlag> TargetTypeFlags ()
+    {
+        yield return TargetTypeFlag.Enemy;
+        yield return TargetTypeFlag.Friendly;
+        yield return TargetTypeFlag.Minion;
+        yield return TargetTypeFlag.Morphid;
+    }
+
+    public override void Apply (string guid)
+    {
+        GameState.RepairGuid(guid, Repair());
+    }
+
+    public override int Cost ()
+    {
+        return 4;
+    }
+
+    public override TargetingType TargetingType ()
+    {
+        return global::TargetingType.Single;
+    }
+}
