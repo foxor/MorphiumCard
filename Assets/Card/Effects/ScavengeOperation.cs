4d475c604480703ee04b7245cd1c8cacb4a34331
Isaac James <izach.james@gmail.com>
2014-03-16 12:30:22 -0700

    Protocol Violation + Scavenge Operation

---
+++ Assets/Card/Effects/ScavengeOperation.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScavengeOperation : Effect {
+    public static DynamicProvider Parts = () => 6;
+
+    public ScavengeOperation(string text) : base(text) {}
+
+    protected override System.Collections.Generic.IEnumerable<DynamicProvider> TemplatingArguments ()
+    {
+        yield return Parts;
+    }
+
+    protected override System.Collections.Generic.IEnumerable<TargetTypeFlag> TargetTypeFlags ()
+    {
+        yield return TargetTypeFlag.Friendly;
+        yield return TargetTypeFlag.Morphid;
+    }
+
+    public override void Apply (string guid)
+    {
+        GameState.AddParts(guid, Parts());
+    }
+
+    public override int Cost ()
+    {
+        return 4;
+    }
+
+    public override TargetingType TargetingType ()
+    {
+        return global::TargetingType.All;
+    }
+}
