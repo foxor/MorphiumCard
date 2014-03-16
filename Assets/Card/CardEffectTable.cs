452348a7e9e6a4a960def1bdfb135a15766ea89e
Isaac James <izach.james@gmail.com>
2014-03-16 10:04:10 -0700

    Permit effects that are in the global namespace

--- Assets/Card/CardEffectTable.cs
+++ Assets/Card/CardEffectTable.cs
@@ -8,13 +8,20 @@ using System.Linq;
 using Assets.Card.Effects;
 
 public class CardEffectTable {
-	public static string TemplateAssemblyName = typeof(ExoTruck).AssemblyQualifiedName.Replace("ExoTruck", "*");
+    public static string NamespacedAssemblyNameTemplate = typeof(ExoTruck).AssemblyQualifiedName.Replace("ExoTruck", "*");
+    public static string GlobalAssemblyNameTemplate = typeof(Consolodate).AssemblyQualifiedName.Replace("Consolodate", "*");
 
     public static Effect Map(string name, string text) {
         name = name.Replace(" ", "");
-		Type t = Type.GetType(TemplateAssemblyName.Replace("*", name));
+        Type t = Type.GetType(NamespacedAssemblyNameTemplate.Replace("*", name));
         if (t == null) {
-            return null;
+            t = Type.GetType(GlobalAssemblyNameTemplate.Replace("*", name));
+            if (t == null) {
+                return null;
+            }
+            else {
+                return (Effect)Activator.CreateInstance(t, text);
+            }
         }
         else {
             return (Effect)Activator.CreateInstance(t, text);
