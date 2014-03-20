25b24ea661b0707bcb46e451a3a2e8a59785cfa2
Isaac James <izach.james@gmail.com>
2014-03-20 07:48:13 -0700

    Localized anomoly

---
+++ Assets/Card/Effects/LocalizedAnomaly.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LocalizedAnomaly : Effect {
+
+    public LocalizedAnomaly(string text) : base(text) {}
+
+    protected override System.Collections.Generic.IEnumerable<DynamicProvider> TemplatingArguments ()
+    {
+        yield break;
+    }
+
+    protected override System.Collections.Generic.IEnumerable<TargetTypeFlag> TargetTypeFlags ()
+    {
+        yield return TargetTypeFlag.Friendly;
+        yield return TargetTypeFlag.Enemy;
+        yield return TargetTypeFlag.Minion;
+    }
+
+    public override void Apply (string guid)
+    {
+        Minion m = GameState.GetMinion(guid);
+        if (m != null) {
+            m.Attack ^= m.Defense;
+            m.Defense ^= m.Attack;
+            m.Attack ^= m.Defense;
+        }
+    }
+
+    public override int Cost ()
+    {
+        return 3;
+    }
+
+    public override TargetingType TargetingType ()
+    {
+        return global::TargetingType.Single;
+    }
+}
