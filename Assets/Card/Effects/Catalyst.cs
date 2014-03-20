e960e44b82f99cd01ae4924ed41065c85957d36d
Isaac James <izach.james@gmail.com>
2014-03-20 12:05:02 -0700

    Catalyst

---
+++ Assets/Card/Effects/Catalyst.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Catalyst : Effect {
+    public static DynamicProvider Engine = () => 2;
+
+    public Catalyst(string text) : base(text) {}
+
+    protected override System.Collections.Generic.IEnumerable<DynamicProvider> TemplatingArguments ()
+    {
+        yield return Engine;
+    }
+
+    protected override System.Collections.Generic.IEnumerable<TargetTypeFlag> TargetTypeFlags ()
+    {
+        yield return TargetTypeFlag.Friendly;
+        yield return TargetTypeFlag.Morphid;
+    }
+
+    public override void Apply (string guid)
+    {
+        GameState.AddEngine(guid, Engine());
+    }
+
+    public override int Cost ()
+    {
+        return 5;
+    }
+
+    public override TargetingType TargetingType ()
+    {
+        return global::TargetingType.All;
+    }
+}
