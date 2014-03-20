3d64e6a3a51c74619c2c05a8ccc55d6121a15c2a
Isaac James <izach.james@gmail.com>
2014-03-20 07:33:46 -0700

    Structural Lattice

---
+++ Assets/Card/Effects/StructuralLattice.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StructuralLattice : Effect {
+    public static DynamicProvider AttackBuff = () => 1;
+    public static DynamicProvider DefenseBuff = () => 5;
+
+    public StructuralLattice(string text) : base(text) {}
+
+    protected override System.Collections.Generic.IEnumerable<DynamicProvider> TemplatingArguments ()
+    {
+        yield return AttackBuff;
+        yield return DefenseBuff;
+    }
+
+    protected override System.Collections.Generic.IEnumerable<TargetTypeFlag> TargetTypeFlags ()
+    {
+        yield return TargetTypeFlag.Friendly;
+        yield return TargetTypeFlag.Minion;
+    }
+
+    public override void Apply (string guid)
+    {
+        GameState.BuffMinion(guid, AttackBuff(), DefenseBuff());
+    }
+
+    public override int Cost ()
+    {
+        return 5;
+    }
+
+    public override TargetingType TargetingType ()
+    {
+        return global::TargetingType.All;
+    }
+}
