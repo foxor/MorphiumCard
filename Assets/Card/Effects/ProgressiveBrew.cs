c726856797b80246762d3b79c7f56157d7014f77
Isaac James <izach.james@gmail.com>
2014-03-20 12:11:39 -0700

    Progressive Brew

---
+++ Assets/Card/Effects/ProgressiveBrew.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProgressiveBrew : Effect {
+    public static DynamicProvider BuffAttack = () => 2;
+    public static DynamicProvider BuffDefense = () => 3;
+
+    public ProgressiveBrew(string text) : base(text) {}
+
+    protected override System.Collections.Generic.IEnumerable<DynamicProvider> TemplatingArguments ()
+    {
+        yield return BuffAttack;
+        yield return BuffDefense;
+    }
+
+    protected override System.Collections.Generic.IEnumerable<TargetTypeFlag> TargetTypeFlags ()
+    {
+        yield return TargetTypeFlag.Friendly;
+        yield return TargetTypeFlag.Morphid;
+    }
+
+    public override void Apply (string guid)
+    {
+        TargetingRequirements subReq = new TargetingRequirements(
+            (int)TargetTypeFlag.Friendly | (int)TargetTypeFlag.Minion,
+            global::TargetingType.All
+        );
+        GameState.AddEngineSequence(guid, () => {
+            foreach (string minionGuid in subReq.AllowedTargets()) {
+                GameState.BuffMinion(minionGuid, BuffAttack(), BuffDefense());
+            }
+        });
+    }
+
+    public override int Cost ()
+    {
+        return 7;
+    }
+
+    public override TargetingType TargetingType ()
+    {
+        return global::TargetingType.All;
+    }
+}
