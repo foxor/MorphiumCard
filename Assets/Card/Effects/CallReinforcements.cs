035f36d38efdba901b8cf3df66f04af1b6f26610
Isaac James <izach.james@gmail.com>
2014-03-21 16:52:09 -0700

    Call Reinforcements

---
+++ Assets/Card/Effects/CallReinforcements.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CallReinforcements : Effect {
+    public static DynamicProvider Attack = () => 2;
+    public static DynamicProvider Defense = () => 1;
+
+    public CallReinforcements(string text) : base(text) {}
+
+    protected override System.Collections.Generic.IEnumerable<DynamicProvider> TemplatingArguments ()
+    {
+        yield return Attack;
+        yield return Defense;
+    }
+
+    protected override System.Collections.Generic.IEnumerable<TargetTypeFlag> TargetTypeFlags ()
+    {
+        yield return TargetTypeFlag.Empty;
+        yield return TargetTypeFlag.Lane;
+    }
+
+    public override void Apply (string guid)
+    {
+        GameState.SummonMinion(guid, Attack(), Defense(), null);
+    }
+
+    public override int Cost ()
+    {
+        return 4;
+    }
+
+    public override TargetingType TargetingType ()
+    {
+        return global::TargetingType.All;
+    }
+}
