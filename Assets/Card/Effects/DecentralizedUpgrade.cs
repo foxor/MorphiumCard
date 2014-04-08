ff8684811b6b208fa89e954abdb381ff23c2b2ca
Isaac James <izach.james@gmail.com>
2014-04-08 07:45:45 -0700

    Fix decentralized upgrade

--- Assets/Card/Effects/DecentralizedUpgrade.cs
+++ Assets/Card/Effects/DecentralizedUpgrade.cs
@@ -8,12 +8,12 @@ public class DecentralizedUpgrade: Effect {
     protected int ConsumedParts = -1;
 
     public DecentralizedUpgrade(string text) : base(text) {
-        Attack = () => ConsumedParts / 3;
-        Defense = () => ConsumedParts;
     }
 
     protected override IEnumerable<DynamicProvider> TemplatingArguments ()
     {
+        Attack = () => ConsumedParts / 3;
+        Defense = () => ConsumedParts;
         yield return Attack;
         yield return Defense;
     }
@@ -26,15 +26,17 @@ public class DecentralizedUpgrade: Effect {
 
     public override void Apply (string guid)
     {
-        if (ConsumedParts == -1) {
-            ConsumedParts = GameState.ActiveMorphid.Parts;
-            GameState.ConsumeParts(GameState.ActiveMorphid.GUID);
-        }
         GameState.BuffMinion(guid, Attack(), Defense());
+    }
+
+    public override void GlobalApply()
+    {
+        GameState.ConsumeParts(GameState.ActiveMorphid.GUID);
     }
 
     public override int Cost ()
     {
+        ConsumedParts = GameState.ActiveMorphid.Parts;
         return 3;
     }
 
