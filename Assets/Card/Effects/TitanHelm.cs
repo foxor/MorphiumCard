bdb440abf5418b45859cfc93ba7d20695dd7042c
Isaac James <izach.james@gmail.com>
2014-04-13 13:48:28 -0700

    Titan Helm

---
+++ Assets/Card/Effects/TitanHelm.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+
+public class TitanHelm: Effect {
+    public static DynamicProvider Durability = () => 20;
+
+    public TitanHelm(string text) : base(text) {}
+
+    protected override IEnumerable<DynamicProvider> TemplatingArguments ()
+    {
+        yield return Durability;
+    }
+
+    protected override IEnumerable<TargetTypeFlag> TargetTypeFlags ()
+    {
+        yield return TargetTypeFlag.Friendly;
+        yield return TargetTypeFlag.Morphid;
+    }
+
+    public override void Apply (string guid)
+    {
+        Action<Damage> onDamage = (Damage damage) => {
+            if (damage.Target == guid && GameState.GetMinion(damage.Source) != null) {
+                GameState.DamageGuid(new Damage() {
+                    Source = guid,
+                    Target = damage.Source,
+                    Magnitude = damage.Magnitude
+                });
+            }
+        };
+
+        GameState.Attach(guid, new Attachment() {
+            Effect = this,
+            RemainingHealth = Durability(),
+            OnDestroy = () => {
+                GameStateWatcher.OnDamage -= onDamage;
+            }
+        }, Slot.Head);
+
+        GameStateWatcher.OnDamage += onDamage;
+    }
+
+    public override int Cost ()
+    {
+        return 6;
+    }
+
+    public override TargetingType TargetingType ()
+    {
+        return global::TargetingType.All;
+    }
+}
