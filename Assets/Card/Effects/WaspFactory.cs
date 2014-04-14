805c3c43b5cc1ae2862120e9d86db9aea7a719e8
Isaac James <izach.james@gmail.com>
2014-04-14 16:28:02 -0700

    Wasp Factory

---
+++ Assets/Card/Effects/WaspFactory.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+
+public class WaspFactory: Effect {
+    public static DynamicProvider Attack = () => 5;
+    public static DynamicProvider Defense = () => 15;
+    public static DynamicProvider WaspAttack = () => 2;
+    public static DynamicProvider WaspDefense = () => 6;
+
+    public WaspFactory(string text) : base(text) {}
+
+    protected override IEnumerable<DynamicProvider> TemplatingArguments ()
+    {
+        yield return Attack;
+        yield return Defense;
+        yield return WaspAttack;
+        yield return WaspDefense;
+    }
+
+    protected override IEnumerable<TargetTypeFlag> TargetTypeFlags ()
+    {
+        yield return TargetTypeFlag.Empty;
+        yield return TargetTypeFlag.Lane;
+    }
+
+    public override void Apply (string guid)
+    {
+        Minion waspFactory = GameState.SummonMinion(guid, Attack(), Defense(), null);
+
+        TargetingRequirements req = new TargetingRequirements(
+            (int)TargetTypeFlag.Random | (int)TargetTypeFlag.Empty | (int)TargetTypeFlag.Lane,
+            global::TargetingType.Single
+        );
+
+        Action<string> onEndTurn = (string morphidGuid) => {
+            if (morphidGuid == waspFactory.MorphidGUID) {
+                foreach (string laneGuid in req.ChosenTargets(null)) {
+                    GameState.SummonMinion(laneGuid, WaspAttack(), WaspDefense(), new MinionBuilder() {
+                        Scrounge = true
+                    });
+                }
+            }
+        };
+
+        waspFactory.OnDeath += () => {
+            GameStateWatcher.OnEndTurn -= onEndTurn;
+        };
+        GameStateWatcher.OnEndTurn += onEndTurn;
+    }
+
+    public override int Cost ()
+    {
+        return 8;
+    }
+
+    public override TargetingType TargetingType ()
+    {
+        return global::TargetingType.Single;
+    }
+}
