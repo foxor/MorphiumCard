80c37d97f7c2d10e6bb3baca343a9959ad56ed57
Isaac James <izach.james@gmail.com>
2014-04-14 16:32:13 -0700

    Reverse Engineer

---
+++ Assets/Card/Effects/ReverseEngineer.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+
+public class ReverseEngineer: Effect {
+    public ReverseEngineer(string text) : base(text) {}
+
+    protected override IEnumerable<DynamicProvider> TemplatingArguments ()
+    {
+        yield break;
+    }
+
+    protected override IEnumerable<TargetTypeFlag> TargetTypeFlags ()
+    {
+        yield return TargetTypeFlag.Enemy;
+        yield return TargetTypeFlag.Friendly;
+        yield return TargetTypeFlag.Minion;
+    }
+
+    public override void Apply (string guid)
+    {
+        Minion target = GameState.GetMinion(guid);
+        if (target != null) {
+            GameState.AddParts(GameState.ActiveMorphid.GUID, target.Defense);
+            GameState.DestroyMinion(target.GUID);
+        }
+    }
+
+    public override int Cost ()
+    {
+        return 7;
+    }
+
+    public override TargetingType TargetingType ()
+    {
+        return global::TargetingType.Single;
+    }
+}
