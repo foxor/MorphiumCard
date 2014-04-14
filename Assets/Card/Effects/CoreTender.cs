9cf0ee38b3853d1f3f3d9262fbd092963b5df426
Isaac James <izach.james@gmail.com>
2014-04-14 15:34:53 -0700

    Core Tender

---
+++ Assets/Card/Effects/CoreTender.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+
+public class CoreTender: Effect {
+    public static DynamicProvider Attack = () => 6;
+    public static DynamicProvider Defense = () => 18;
+    public static DynamicProvider Engine = () => 1;
+
+    public CoreTender(string text) : base(text) {}
+
+    protected override IEnumerable<DynamicProvider> TemplatingArguments ()
+    {
+        yield return Attack;
+        yield return Defense;
+        yield return Engine;
+    }
+
+    protected override IEnumerable<TargetTypeFlag> TargetTypeFlags ()
+    {
+        yield return TargetTypeFlag.Empty;
+        yield return TargetTypeFlag.Lane;
+    }
+
+    public override void Apply (string guid)
+    {
+        GameState.SummonMinion(guid, Attack(), Defense(), null);
+        GameState.AddEngine(GameState.ActiveMorphid.GUID, Engine());
+    }
+
+    public override int Cost ()
+    {
+        return 8;
+    }
+
+    public override TargetingType TargetingType ()
+    {
+        return global::TargetingType.Single;
+    }
+}
